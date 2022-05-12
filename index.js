be5b500736166ee08d424c64af39793f3371567a
CaioOta <caio.ryou@gmail.com>
2022-05-12 09:39:41 -0300

    feat: adicionando um arquivo HTML

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>pagina inicial</h1>'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'views/index.html'))
+)
 
 app.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
